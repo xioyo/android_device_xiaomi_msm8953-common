72f57c86b0f2fd40b5b9790098654c94f5f6c0d8
Lunarixus <Nathanb644@hotmail.co.uk>
2020-10-01 16:41:11 +0000

    msm8953-common: Remove packages at build time

--- msm8953.mk
+++ msm8953.mk
@@ -305,6 +305,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     librmnetctl \
